5f377dd0a90a494c1a21c1e13cb97fa281823224
David Jones <dxjones@gmail.com>
2011-05-24 17:27:53 -0400

    added debug info to index.php

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<? // index.php
+$time['start'] = microtime(TRUE);
+$cr = "\n";
+?>
 <!doctype html>
 <html>
 <head>
@@ -6,5 +10,20 @@
 <body>
 <h1>Hello World!</h1>
 <p>This is a quickstart demo.</p>
+<?
+echo '<pre>' . $cr;
+echo 'Debug Info:' . $cr . $cr;
+$list = array('_SERVER', '_GET', '_POST', '_REQUEST', '_FILES', '_COOKIE', '_SESSION', '_ENV');
+foreach ($list as $var) {
+	if (isset($$var)) {
+		echo '$' . $var . ' =' . $cr;
+		print_r($$var);
+		echo '- - - - -' . $cr . $cr;
+	}
+}
+echo '</pre>' . $cr;
+$time['elapsed'] = microtime(TRUE) - $time['start'];
+echo '<div align="right">elapsed time = ' . $sprintf('%.3f', $time['elapsed']) . '</div>' . $cr;
+?>
 </body>
 </html>
