6889ca35e7ab7b0892edcc3bdb523288199ba295
David Jones <dxjones@gmail.com>
2011-06-04 11:44:13 -0400

    added test code from Mark Parsons

---
+++ mark.php
@@ -0,0 +1,21 @@
+<? // mark.php
+echo '<p>Mark's test code, ...</p>';
+
+echo '<b>$_SERVER:</b><br/><br/>';
+foreach ($_SERVER as $k => $v){
+    echo '&nbsp;&nbsp;&nbsp;&nbsp;' . $k .'  :  ' . $v . '<br/>';
+}
+
+
+echo '<br/><br/><b>$_REQUEST:</b><br/>';
+foreach ($_REQUEST as $k => $v){
+    echo '&nbsp;&nbsp;&nbsp;&nbsp;' . $k .'  :  ' . $v .  '<br/>';
+}
+
+echo '<br/><br/><b>$_ENV:</b><br/>';
+foreach ($_ENV as $k => $v){
+    echo '&nbsp;&nbsp;&nbsp;&nbsp;' . $k .'  :  ' . $v . '<br/>';
+}
+
+
+?>
